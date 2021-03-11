c4475bae26c6ee941aaaec545df2f170423e1456
pofider <honza.pofider@seznam.cz>
2021-03-11 14:24:45 +0100

    fix bugs in filtering

--- test/queryTest.js
+++ test/queryTest.js
@@ -31,6 +31,18 @@ describe('query', function () {
     }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[_id] = @1)')
   })
 
+  it('should support filtering and make AND from multiple properties on filter', function () {
+    query(table, {
+      $filter: { int: 1, bool: true }
+    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE (([UserType].[int] = @1) AND ([UserType].[bool] = @2))')
+  })
+
+  it('should support filtering and make AND when second property is $or', function () {
+    query(table, {
+      $filter: { int: 1, $or: [{ bool: true }] }
+    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE (([UserType].[int] = @1) AND ([UserType].[bool] = @2))')
+  })
+
   it('should support projection', function () {
     query(table, {
       $select: { int: 1 }
@@ -50,20 +62,82 @@ describe('query', function () {
   })
 
   it('should support filter on complex props', function () {
-    query(table, {
-      $filter: { address: { street: 'foo ' } }
-    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[address_street] = @1)')
+    const r = query(table, {
+      $filter: { address: { street: 'foo' } }
+    }, 'users', model)
+
+    r.text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[address_street] = @1)')
+    r.values[0].should.be.eql('foo')
   })
 
-  it('should support is null filters for null', function () {
+  it('should support is null filters', function () {
+    query(table, {
+      $filter: { int: null }
+    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[int] IS NULL)')
+  })
+
+  it('should support is null filters for undefined', function () {
+    query(table, {
+      $filter: { int: undefined }
+    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[int] IS NULL)')
+  })
+
+  it('should support is null filters for null on complex', function () {
     query(table, {
       $filter: { address: null }
     }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE (([UserType].[address_street] IS NULL) AND ([UserType].[address_number] IS NULL))')
   })
 
-  it('should support is null filters for undefined', function () {
+  it('should support is null filters for undefined on complex', function () {
     query(table, {
       $filter: { address: undefined }
     }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE (([UserType].[address_street] IS NULL) AND ([UserType].[address_number] IS NULL))')
+  })
+
+  it('should support $in', function () {
+    const r = query(table, {
+      $filter: { int: { $in: [1, 2] } }
+    }, 'users', model)
+    r.text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[int] IN (@1, @2))')
+    r.values[0].should.be.eql(1)
+    r.values[1].should.be.eql(2)
+  })
+
+  it('should support $in with no values', function () {
+    query(table, {
+      $filter: { int: { $in: [] } }
+    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE (1=0)')
+  })
+
+  it('should support $lt', function () {
+    query(table, {
+      $filter: { int: { $lt: 2 } }
+    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[int] < @1)')
+  })
+
+  it('should support $lte', function () {
+    query(table, {
+      $filter: { int: { $lte: 2 } }
+    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[int] <= @1)')
+  })
+
+  it('should support $gte', function () {
+    query(table, {
+      $filter: { int: { $gt: 2 } }
+    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[int] > @1)')
+  })
+
+  it('should support $gte', function () {
+    query(table, {
+      $filter: { int: { $gte: 2 } }
+    }, 'users', model).text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[int] >= @1)')
+  })
+
+  it('should support filter and lt on complex props', function () {
+    const r = query(table, {
+      $filter: { address: { number: { $lt: 1 } } }
+    }, 'users', model)
+    r.text.should.be.eql('SELECT [UserType].* FROM [UserType] WHERE ([UserType].[address_number] < @1)')
+    r.values[0].should.be.eql(1)
   })
 })
